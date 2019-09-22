34fdb94957c545741f63ed8862727cec3c546e61
germmand <german.aguilera17@gmail.com>
2019-09-22 17:35:47 -0400

    Initial Setup.

---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+import Enzyme from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
+import Adapter from 'enzyme-adapter-react-16'; // eslint-disable-line import/no-extraneous-dependencies
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
