b5903f48b439f80ff32f48c155e3e7e55c800be6
germmand <german.aguilera17@gmail.com>
2019-09-25 11:10:41 -0400

    refactor: Moving list of products into its own package.

--- src/Pages/MainPage/index.js
+++ src/Pages/MainPage/index.js
@@ -7,6 +7,8 @@ import Grid from '@material-ui/core/Grid';
 
 import styles from './styles';
 
+import products from '../../Products';
+
 import Components from '../../Components';
 const { 
   BezosHeader, 
@@ -14,52 +16,6 @@ const {
   Product
 } = Components;
 
-const items = [{
-    id: 1,
-    name: 'Burger',
-    price: 5,
-    image: '/images/items/burger.jpg',
-}, {
-    id: 2,
-    name: 'Book',
-    price: 16,
-    image: '/images/items/book.jpg',
-}, {
-    id: 3,
-    name: 'Skateboard',
-    price: 150,
-    image: '/images/items/skateboard.jpg',
-}, {
-    id: 4,
-    name: 'Gaming Console',
-    price: 250,
-    image: '/images/items/ps4.jpg',
-}, {
-    id: 5,
-    name: 'Drone',
-    price: 350,
-    image: '/images/items/drone.jpg',
-}, {
-    id: 6,
-    name: '4K TV',
-    price: 900,
-    image: '/images/items/4ktv.jpg',
-}, {
-    id: 7,
-    name: 'Smartphone',
-    price: 1000,
-    image: '/images/items/smartphone.jpg',
-}, {
-    id: 8,
-    name: 'Rolex',
-    price: 9000,
-    image: '/images/items/rolex.jpg',
-}, {
-    id: 9,
-    name: 'Lamborghini',
-    price: 200000,
-    image: '/images/items/lamborghini.jpg',
-}];
 
 export class MainPage extends React.Component {
     state = {
@@ -73,9 +29,9 @@ export class MainPage extends React.Component {
           <MoneyLeftWrapper />
           {/* Items */}
           <Grid container spacing={4}>
-            {items.map(item => (
-              <Grid item key={item.id} xs={12} sm={6} md={4}>
-                <Product item={item} />
+            {products.map(product => (
+              <Grid item key={product.id} xs={12} sm={6} md={4}>
+                <Product item={product} />
               </Grid>
             ))}
           </Grid>
---
+++ src/Products/index.js
@@ -0,0 +1,51 @@
+const products = [{
+    id: 1,
+    name: 'Burger',
+    price: 5,
+    image: '/images/items/burger.jpg',
+}, {
+    id: 2,
+    name: 'Book',
+    price: 16,
+    image: '/images/items/book.jpg',
+}, {
+    id: 3,
+    name: 'Skateboard',
+    price: 150,
+    image: '/images/items/skateboard.jpg',
+}, {
+    id: 4,
+    name: 'Gaming Console',
+    price: 250,
+    image: '/images/items/ps4.jpg',
+}, {
+    id: 5,
+    name: 'Drone',
+    price: 350,
+    image: '/images/items/drone.jpg',
+}, {
+    id: 6,
+    name: '4K TV',
+    price: 900,
+    image: '/images/items/4ktv.jpg',
+}, {
+    id: 7,
+    name: 'Smartphone',
+    price: 1000,
+    image: '/images/items/smartphone.jpg',
+}, {
+    id: 8,
+    name: 'Rolex',
+    price: 9000,
+    image: '/images/items/rolex.jpg',
+}, {
+    id: 9,
+    name: 'Lamborghini',
+    price: 200000,
+    image: '/images/items/lamborghini.jpg',
+}];
+
+// We want it to be shown by price in ascending order.
+products.sort((a, b) => a.price - b.price);
+
+export default products;
