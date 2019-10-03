28403a5eb0cb6db1f6431b84b7016ada8037b4f2
theREALaltoid <oronazac@protonmail.com>
2019-10-03 14:35:33 -0700

    Fixed Width Error

--- src/Pages/MainPage/styles.js
+++ src/Pages/MainPage/styles.js
@@ -1,16 +1,19 @@
 export default () => ({
-    root: {
-        flexGrow: 1,
-        paddingTop: '3%',
-        margin: '0 auto',
-        width: '70%',
-    },
-    paper: {
-        padding: '2%',
-        marginTop: 15,
-        marginBottom: 10,
-    },
-    fullWidth: {
-        width: '100%'
-    },
+  root: {
+    flexGrow: 1,
+    paddingTop: '3%',
+    margin: '0 auto',
+    width: '70%',
+    '@media (max-width: 750px)': {
+      width: '100%'
+    }
+  },
+  paper: {
+    padding: '2%',
+    marginTop: 15,
+    marginBottom: 10
+  },
+  fullWidth: {
+    width: '100%'
+  }
 });
