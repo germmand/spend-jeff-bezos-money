127c6730ccec2d47609221ce56ef2846083a3eae
theREALaltoid <oronazac@protonmail.com>
2019-10-03 22:00:12 -0700

    Add Product

--- src/Products/index.js
+++ src/Products/index.js
@@ -1,49 +1,65 @@
-const products = [{
+const products = [
+  {
     id: 1,
     name: 'Burger',
     price: 5,
-    image: '/images/items/burger.jpg',
-}, {
+    image: '/images/items/burger.jpg'
+  },
+  {
     id: 2,
     name: 'Book',
     price: 16,
-    image: '/images/items/book.jpg',
-}, {
+    image: '/images/items/book.jpg'
+  },
+  {
     id: 3,
     name: 'Skateboard',
     price: 150,
-    image: '/images/items/skateboard.jpg',
-}, {
+    image: '/images/items/skateboard.jpg'
+  },
+  {
     id: 4,
     name: 'Gaming Console',
     price: 250,
-    image: '/images/items/ps4.jpg',
-}, {
+    image: '/images/items/ps4.jpg'
+  },
+  {
     id: 5,
     name: 'Drone',
     price: 350,
-    image: '/images/items/drone.jpg',
-}, {
+    image: '/images/items/drone.jpg'
+  },
+  {
     id: 6,
     name: '4K TV',
     price: 900,
-    image: '/images/items/4ktv.jpg',
-}, {
+    image: '/images/items/4ktv.jpg'
+  },
+  {
     id: 7,
     name: 'Smartphone',
     price: 1000,
-    image: '/images/items/smartphone.jpg',
-}, {
+    image: '/images/items/smartphone.jpg'
+  },
+  {
     id: 8,
     name: 'Rolex',
     price: 9000,
-    image: '/images/items/rolex.jpg',
-}, {
+    image: '/images/items/rolex.jpg'
+  },
+  {
     id: 9,
     name: 'Lamborghini',
     price: 200000,
-    image: '/images/items/lamborghini.jpg',
-}];
+    image: '/images/items/lamborghini.jpg'
+  },
+  {
+    id: 10,
+    name: 'Gulfstream G450',
+    price: 38000000,
+    image: 'images/items/Gulfstream-G450.jpg'
+  }
+];
 
 // We want it to be shown by price in ascending order.
 products.sort((a, b) => a.price - b.price);
