d4a4244a3997d514d2b72f7a34d919d7d272211e
panda <25163032+theREALaltoid@users.noreply.github.com>
2019-10-03 20:56:47 -0700

    Fixed issue where total added can be negative

--- src/Components/Product/index.js
+++ src/Components/Product/index.js
@@ -57,12 +57,21 @@ export class ProductRaw extends React.Component {
 
   onButtonSell = value => () => {
     const { quantity, toBuyquantity } = this.state;
-    this.setState(
-      {
-        quantity: quantity - toBuyquantity
-      },
-      this.onProductUpdated
-    );
+    if (quantity - toBuyquantity < 0) {
+      this.setState(
+        {
+          quantity: 0
+        },
+        this.onProductUpdated
+      );
+    } else {
+      this.setState(
+        {
+          quantity: quantity - toBuyquantity
+        },
+        this.onProductUpdated
+      );
+    }
   };
 
   render() {
